b96889b781aab5b7f6dbf9a941f11ce725d3b8f7
suraj <agnibyte@gmail.com>
2024-11-24 17:39:32 +0530

    dynamic doc tabel

--- src/pages/docTable.js
+++ src/pages/docTable.js
@@ -1,24 +1,49 @@
 import DocumentTable from "@/components/tabels/documentTable";
 
 export default function DocTable() {
-  const headCells = [
-    { id: "masterNo", numeric: false, disablePadding: false, label: "Master No." },
-    { id: "vehicleNo", numeric: true, disablePadding: false, label: "Vehicle No." },
-    { id: "fat", numeric: true, disablePadding: false, label: "Fat" },
-    { id: "carbs", numeric: true, disablePadding: false, label: "Carbs" },
-    { id: "protein", numeric: true, disablePadding: false, label: "Protein" },
-  ];
-  
-  const rows = [
-    { id: 1, masterNo: "M123", vehicleNo: "A1", fat: 5.2, carbs: 30, protein: 10 },
-    { id: 2, masterNo: "M124", vehicleNo: "A2", fat: 4.5, carbs: 32, protein: 12 },
-  ];
-  
+  const tableData = {
+    headCells: [
+      {
+        id: "masterNo",
+        numeric: false,
+        disablePadding: false,
+        label: "Master No.",
+      },
+      {
+        id: "vehicleNo",
+        numeric: true,
+        disablePadding: false,
+        label: "Vehicle No.",
+      },
+      { id: "fat", numeric: true, disablePadding: false, label: "Fat" },
+      { id: "carbs", numeric: true, disablePadding: false, label: "Carbs" },
+      { id: "protein", numeric: true, disablePadding: false, label: "Protein" },
+    ],
+    rows: [
+      {
+        id: 1,
+        masterNo: "M123",
+        vehicleNo: "A1",
+        fat: 5.2,
+        carbs: 30,
+        protein: 10,
+      },
+      {
+        id: 2,
+        masterNo: "M124",
+        vehicleNo: "A2",
+        fat: 4.5,
+        carbs: 32,
+        protein: 12,
+      },
+    ],
+  };
+
   return (
     <div>
       <DocumentTable
-        rows={rows}
-        headCells={headCells}
+        rows={tableData.rows}
+        headCells={tableData.headCells}
         title="Nutrition Information"
       />
     </div>

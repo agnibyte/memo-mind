cb7efbda7f8f3cf9261cf3cd292c5a6ebd1799b3
Suraj <surajsangle00@gmail.com>
2025-01-02 23:41:28 +0530

    api check

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.BASE_PATH || "",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;

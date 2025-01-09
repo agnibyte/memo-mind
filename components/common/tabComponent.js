1bafc144f01f58ff3f80ad3b97354d7a2882e132
Suraj <surajsangle00@gmail.com>
2025-01-09 07:18:58 +0530

    ta new component

---
+++ components/common/tabComponent.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styles from "@/styles/common/tabComponent.module.scss";
+
+
+const tabsData = [
+  { id: "one", label: "One" },
+  { id: "two", label: "Two" },
+  { id: "three", label: "Three" },
+  { id: "four", label: "Four" }
+];
+
+const TabComponent = () => {
+  const [activeTab, setActiveTab] = useState("one");
+
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+
+  return (
+    <div className={`${styles.tabs} ${styles.group}`}>
+      {tabsData.map((tab) => (
+        <div
+          key={tab.id}
+          className={activeTab === tab.id ? styles.active : ""}
+          onClick={() => handleTabClick(tab.id)}
+        >
+          <a href={`#${tab.id}`}>{tab.label}</a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TabComponent;
+

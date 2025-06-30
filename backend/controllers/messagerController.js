251905b89da6e4565272c33f9dbd74ade417b9f3
Suraj <surajsangle00@gmail.com>
2025-06-30 23:16:10 +0530

    Add sendMessage function to handle message sending via Twilio API

---
+++ backend/controllers/messagerController.js
@@ -0,0 +1,47 @@
+// import { postSiteApiData } from "@/utilities/services/apiService";
+import { Twilio } from "twilio";
+
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+
+const client = new Twilio(accountSid, authToken);
+
+export default async function sendMessage(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
+  const { message, contacts } = req.body;
+
+  if (
+    !message ||
+    !contacts ||
+    !Array.isArray(contacts) ||
+    contacts.length === 0
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Message and at least one contact are required." });
+  }
+
+  try {
+    for (const contact of contacts) {
+      await client.messages.create({
+        body: message,
+        from: "+12694823264", // your Twilio number
+        to: contact.trim(),
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Messages sent successfully" });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    return res.status(500).json({
+      success: false,
+      error: "Failed to send message",
+      details: error.message,
+    });
+  }
+}
